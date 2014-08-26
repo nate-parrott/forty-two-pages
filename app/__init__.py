834edbb4e55c63052eafd2853a68d6dc51845acf
nate-parrott <nateparro2t@gmail.com>
2014-08-26 13:18:56 -0400

    gheognio

--- app/__init__.py
+++ app/__init__.py
@@ -1,33 +1,37 @@
 import os
-from flask import Flask
-import pymongo
+
+# new relic:
 import newrelic.agent
 newrelic.agent.initialize('newrelic.ini')
 
+# flask:
+from flask import Flask
 class Flask42(Flask):
     def get_send_file_max_age(self, name):
 		if app.debug:
 			return 0
 		else:
 			return Flask.get_send_file_max_age(self, name)
-
 app = Flask42(__name__)
 app.secret_key = "94nY3,R83nf#8qjq02@^ frwfjiwHFEhe028.dfumf2x 0d"
-app.config['MAX_CONTENT_LENGTH'] = 512 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
 
 # production logging:
 import logging
 from logging import StreamHandler
 file_handler = StreamHandler()
-app.logger.setLevel(logging.DEBUG)  # set the desired logging level here
+app.logger.setLevel(logging.DEBUG)
 app.logger.addHandler(file_handler)
 
+# templating:
 from jinja2 import Environment, PackageLoader
 env = Environment(loader=PackageLoader('app', 'templates'))
 env.autoescape = True
 def templ8(name, vars):
 	return env.get_template(name).render(vars)
 
+# mongodb:
+import pymongo
 if 'MONGOHQ_URL' in os.environ:
 	db = pymongo.MongoClient(os.environ['MONGOHQ_URL']).app28469442
 else:
@@ -35,6 +39,7 @@ else:
 db.sites.ensure_index("name", background=True)
 db.pages.ensure_index("name", background=True)
 
+# import pages:
 import edit
 import upload
 import settings
